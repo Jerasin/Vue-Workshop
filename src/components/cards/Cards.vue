e85671a922d424b411b1a08db55e13a747b91b21
Jerasin <choi.jerasin@gmail.com>
2021-07-05 10:19:42 +0700

    Edit ui

---
+++ src/components/cards/Cards.vue
@@ -0,0 +1,248 @@
+<template>
+  <div class="container">
+    <!-- col-sm-3 -->
+    <div class="row justify-content-md-center align-items-center">
+      <div class="col-auto col-xl-3 col-xxl-3 p-0 align-self-center box">
+        <div class="box-total-text">
+          <h3>Total</h3>
+        </div>
+
+        <div class="box-total-img d-flex justify-content-center">
+          <img class="img" src="@/assets/shopping-cart.png" alt="" />
+        </div>
+
+        <p>18</p>
+      </div>
+
+      <div class="col-auto col-xl-3 col-xxl-3 p-0 align-self-center box">
+        <div class="box-soldout-text">
+          <h3>Sold-Out</h3>
+        </div>
+
+        <div class="box-soldout-img d-flex justify-content-center">
+          <img class="img" src="@/assets/sold-out.png" alt="" />
+        </div>
+
+        <p>18</p>
+      </div>
+
+      <div class="col-auto col-xl-3 col-xxl-3 p-0 align-self-center box">
+        <div class="box-return-text">
+          <h3>Return</h3>
+        </div>
+
+        <div class="box-return-img d-flex justify-content-center">
+          <img class="img" src="@/assets/return.png" alt="" />
+        </div>
+
+        <p>18</p>
+      </div>
+
+      <div class="col-auto col-xl-3 col-xxl-3 p-0 align-self-center box">
+        <div class="box-discount-text">
+          <h3>Discount</h3>
+        </div>
+
+        <div class="box-discount-img d-flex justify-content-center">
+          <img class="img" src="@/assets/discount.png" alt="" />
+        </div>
+
+        <p>18</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cards",
+};
+</script>
+
+<style scoped>
+.img-sale {
+  width: 120px;
+  border: 4px solid red;
+}
+
+.box {
+  position: relative;
+  display: flex;
+}
+
+.box-total-text {
+  width: 100%;
+}
+
+.box-total-img {
+  width: 100%;
+  background-color: rgb(5, 255, 5);
+  border : 2px solid black;
+  margin: 3px;
+}
+
+.box-soldout-text {
+  width: 100%;
+}
+
+.box-soldout-img {
+  width: 100%;
+  background-color: orange;
+  border : 2px solid black;
+  margin: 3px;
+}
+
+.box-return-text {
+  width: 100%;
+}
+
+.box-return-img {
+  width: 100%;
+  background-color: red;
+  border : 2px solid black;
+  margin: 3px;
+}
+
+.box-discount-text {
+  width: 100%;
+}
+
+.box-discount-img {
+  width: 100%;
+  background-color: rgb(0, 229, 255);
+  border : 2px solid black;
+  margin: 3px;
+}
+
+p {
+  position: absolute;
+  top: 35%;
+  left: 10%;
+  font-size: 20px;
+}
+
+/* .row {
+  border: 5px solid red;
+} */
+
+.col-auto {
+  border: 2px solid rgb(0, 0, 0);
+  padding-bottom: 10px;
+}
+
+.img {
+  display: inline;
+  width: 100%;
+  max-width: 100px;
+}
+
+.img-shopping-cart {
+  width: 100%;
+  max-width: 100px;
+}
+
+@media only screen and (max-width: 1295px) {
+  .img {
+    display: inline;
+    width: 100%;
+    max-width: 80px;
+  }
+}
+
+@media only screen and (max-width: 1200px) {
+  .col-auto {
+    width: 25%;
+  }
+}
+
+@media only screen and (max-width: 1090px) {
+  .col-auto {
+    width: 50%;
+  }
+}
+
+@media only screen and (max-width: 1050px) {
+  .img {
+    display: inline;
+    width: 100%;
+    max-width: 75px;
+  }
+}
+
+@media only screen and (max-width: 1022px) {
+  .col {
+    width: 200px;
+  }
+}
+
+@media only screen and (max-width: 837px) {
+  .col-auto {
+    width: 50%;
+  }
+}
+
+@media only screen and (max-width: 797px) {
+  .col-auto {
+    width: 50%;
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .col-auto {
+    width: 50%;
+  }
+}
+
+@media only screen and (max-width: 704px) {
+  .col-auto {
+    width: 50%;
+  }
+}
+
+@media only screen and (max-width: 680px) {
+  .col-auto {
+    width: 100%;
+  }
+
+}
+
+
+@media only screen and (max-width: 500px) {
+  h3 {
+    font-size: 23px;
+  }
+}
+
+@media only screen and (max-width: 400px) {
+  h3 {
+    font-size: 20px;
+  }
+
+  /* p {
+    top: 58%;
+  } */
+}
+
+@media only screen and (max-width: 357px) {
+  h3 {
+    font-size: 20px;
+  }
+}
+
+@media only screen and (max-width: 326px) {
+  h3 {
+    width: 30px;
+    font-size: 18px;
+  }
+
+  .img {
+    display: inline;
+    width: 100%;
+    max-width: 65px;
+  }
+
+  p {
+    top: 55%;
+  }
+}
+</style>
--- src/components/core/Sidebar.vue
+++ src/components/core/Sidebar.vue
@@ -19,7 +19,7 @@
           <div
             class="
               fs-5
-              d-none d-sm-inline d-flex
+              d-sm-inline d-flex
               align-items-center
               pb-3
               mb-md-0
@@ -32,7 +32,6 @@
             <b>Menu</b>
           </div>
           <!--End buttom Meun -->
-
 
           <!-- Start List Menu -->
           <ul
@@ -49,7 +48,10 @@
               <div data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                 <router-link class="text-decoration-none" to="/stock">
                   <div class="row">
-                    <div class="col icon"><i class="fas fa-warehouse" /></div>
+                    <div class="col icon hover">
+                      <i class="fas fa-warehouse" />
+                      <span class="tooltiptext">Stock</span>
+                    </div>
                     <div class="col d-none d-sm-block">
                       <span class="ms-1 d-none d-sm-inline text-warning">
                         Stock
@@ -66,7 +68,10 @@
               <div class="nav-link px-0 align-middle">
                 <router-link class="text-decoration-none" to="/report">
                   <div class="row">
-                    <div class="col icon"><i class="fas fa-file-alt" /></div>
+                    <div class="col icon hover">
+                      <i class="fas fa-file-alt" />
+                      <span class="tooltiptext">Report</span>
+                    </div>
                     <div class="col d-none d-sm-block">
                       <span class="ms-1 d-none d-sm-inline text-warning">
                         Report
@@ -83,7 +88,10 @@
               <div data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                 <router-link class="text-decoration-none" to="/about">
                   <div class="row">
-                    <div class="col icon"><i class="fas fa-book-open" /></div>
+                    <div class="col icon hover">
+                      <i class="fas fa-book-open" />
+                      <span class="tooltiptext">About</span>
+                    </div>
                     <div class="col d-none d-sm-block">
                       <span class="ms-1 d-none d-sm-inline text-warning">
                         About
@@ -100,7 +108,10 @@
               <div data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                 <router-link class="text-decoration-none" to="/users">
                   <div class="row">
-                    <div class="col icon"><i class="fas fa-users me-2" /></div>
+                    <div class="col icon hover">
+                      <i class="fas fa-users me-2" />
+                      <span class="tooltiptext">User</span>
+                    </div>
                     <div class="col d-none d-sm-block">
                       <span class="ms-1 d-none d-sm-inline text-warning"
                         >User
@@ -116,12 +127,13 @@
             <li>
               <div data-bs-toggle="collapse" class="nav-link px-0 align-middle">
                 <div class="row">
-                  <div class="col icon" @click="isLogOut">
-                    <i class="fas fa-sign-out-alt me-2 logout"/>
+                  <div class="col icon hover" @click="isLogOut">
+                    <i class="fas fa-sign-out-alt me-2 logout" />
+                    <span class="tooltiptext">Logout</span>
                   </div>
-                  <div class="col d-none d-sm-block">
+                  <div class="col d-none test d-sm-block">
                     <span
-                      class="ms-1 d-none d-sm-inline text-warning logout"
+                      class="ms-1 d-sm-inline text-warning logout"
                       @click="isLogOut"
                     >
                       Logout
@@ -172,9 +184,29 @@ export default {
   cursor: pointer;
 }
 
+.tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: rgba(0, 0, 0, 0.476);
+  color: rgb(201, 201, 201);
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px 0;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 999;
+}
+
 .icon {
   width: 45px;
   text-align: start;
   padding: 0px 10px;
+}
+
+@media only screen and (max-width: 400px) {
+  .hover:hover .tooltiptext {
+    visibility: visible;
+  }
 }
 </style>
